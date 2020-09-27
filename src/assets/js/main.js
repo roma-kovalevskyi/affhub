5088180cf2b536b1fd423086334805126cc9ce8c
Roman <roma.kovalevskyi@gmail.com>
2020-09-27 23:22:47 +0300

    Added Video and Longread pages. Refactored some css code.

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -19,7 +19,59 @@ $(document).ready(function() {
         }
     });
 
-    // Select answer options
+    // Play youtube video
+    function youtubeShowVideo() {
+        let videoButton, youtube;
+        videoButton = document.getElementsByClassName("youtube");
+        for (let i = 0; i < videoButton.length; i++) {
+            youtube = videoButton[i];
+            youtube.onclick = function() {
+                let iframe = document.createElement("iframe");
+                iframe.setAttribute(
+                    "src",
+                    "https://www.youtube.com/embed/" +
+                    this.id +
+                    "?autoplay=1&autohide=1&border=0&wmode=opaque&enablejsapi=1"
+                );
+                iframe.setAttribute('allow', 'autoplay');
+                iframe.style.width = this.style.width;
+                iframe.style.height = this.style.height;
+                this.parentNode.replaceChild(iframe, this);
+            };
+        }
+    }
+    youtubeShowVideo();
+
+    function onPlayerReady(event) {
+        event.target.playVideo();
+    }
+
+    $(".youtube").click(function(e) {
+        e.preventDefault();
+
+        let player;
+
+        function onYouTubeIframeAPIReady(id) {
+            player = new YT.Player(id, {
+                videoId: id,
+                events: {
+                    onReady: onPlayerReady,
+                },
+            });
+        }
+        const _this = $(this);
+        const idYoutube = _this.attr("id");
+
+        onYouTubeIframeAPIReady(idYoutube);
+    });
+
+    let tag = document.createElement("script");
+
+    tag.src = "https://www.youtube.com/iframe_api";
+    let firstScriptTag = document.getElementsByTagName("script")[0];
+    firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+    // Test. Select answer options
     $('.answer-option__checkbox').on('click', function() {
         $('.answer-option').removeClass('selected').addClass('disabled');
         $(this).parent().removeClass('disabled').addClass('selected');
@@ -47,9 +99,28 @@ $(document).ready(function() {
         }]
     });
 
+    // Longread slider
+    $('.longread-slider').slick({
+        infinite: true,
+        dots: true,
+        customPaging: function(slider, i) {
+            return '<button type="button" data-role="none" role="button" tabindex="0"><img src="img/longread-slider-photo-sm-' + (i + 1) + '.jpg" srcset="img/longread-slider-photo-sm-' + (i + 1) + '@2x.jpg" class="photo-image"></button>';
+        },
+        appendDots: $('.longread__slider-box'),
+        appendArrows: $('.longread__slider-box'),
+        prevArrow: '<button type="button" class="slick-arrow slick-prev"><img src="img/longread-slider-arrow-l.svg" alt=""></button>',
+        nextArrow: '<button type="button" class="slick-arrow slick-next"><img src="img/longread-slider-arrow-r.svg" alt=""></button>',
+        responsive: [{
+            breakpoint: 1023,
+            settings: {
+                arrows: false
+            }
+        }]
+    });
+
     // Copy referal-link to clipboard
     $('.partner-product__copy-link').on('click', function() {
-        var element = $("<input>");
+        let element = $("<input>");
 
         element.val($('.partner-product__referal-link').text());
         $("body").append(element);
